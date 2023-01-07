5b868f5606e3e7f42ef41091f02c47716340558e
rubenpena64 <109831018+rubenpena64@users.noreply.github.com>
2023-01-07 10:12:04 -0300

    Solo para actualizar koteb por fallos

--- src/main/java/com/porfolio/backend/controller/AcercaController.java
+++ src/main/java/com/porfolio/backend/controller/AcercaController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Ruben
  */
+
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
 public class AcercaController {
